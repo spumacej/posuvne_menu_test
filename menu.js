b5bae5f7ee3a0390ffd56b967243d4bd52c00f17
spumacej <zeleold@gmail.com>
2025-04-21 19:35:17 +0200

    rigged js script

--- menu.js
+++ menu.js
@@ -3,10 +3,22 @@ let menu_default = document.getElementById("menu_default");
 window.addEventListener("scroll", function() {
     if (window.scrollY > 99) {
         menu_default.style.opacity = "1";
-    } else {
-        menu_default.style.opacity = "0"; 
+        menu_default.style.transform = "TranslateY(0)";
+    } 
+    else {
+        menu_default.style.opacity = "0";
     }
 });
+window.addEventListener("scroll", function(){
+    if(window.scrollY < 99) {
+        menu_default.style.transform = "TranslateY(-50)"
+    }
+    else {
+        menu_default.style.transform = "TranslateY(0)";
+    }
+});
+
+
 
 // let obrazek1 = document.getElementById("obrazek1");
 
