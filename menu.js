ddc12051a717062d1ca4e4135306cbe0b538eeb3
Christfer7 <christopher.klempa@gmail.com>
2025-04-21 19:51:48 +0200

    menu

--- menu.js
+++ menu.js
@@ -1,32 +1,28 @@
+// Posuvanie menu dole a hore
+
 let menu_default = document.getElementById("menu_default");
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     if (window.scrollY > 99) {
         menu_default.style.opacity = "1";
-        menu_default.style.transform = "TranslateY(0)";
-    } 
-    else {
+        menu_default.style.transform = "translateY(0)";
+    } else {
         menu_default.style.opacity = "0";
+        menu_default.style.transform = "translateY(-50px)";
     }
 });
-window.addEventListener("scroll", function(){
-    if(window.scrollY < 99) {
-        menu_default.style.transform = "TranslateY(-50)"
+
+
+
+// Posuvanie obrazku z lava 
+
+let obrazek1 = document.getElementById("obrazek1");
+
+window.addEventListener("scroll", function (){
+    if (window.scrollY >= 500) {
+        obrazek1.style.animation = true;
     }
     else {
-        menu_default.style.transform = "TranslateY(0)";
+        obrazek1.style.animation = false;
     }
 });
-
-
-
-// let obrazek1 = document.getElementById("obrazek1");
-
-// window.addEventListener("scroll", function (){
-//     if (window.scrollY >= 500) {
-//         obrazek1.style.animation = true;
-//     }
-//     else {
-//         obrazek1.style.animation = false;
-//     }
-// });
