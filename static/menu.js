ae42c7dc6ed9156f73106dfc9a1acbc8164ab5f0
Karosene <adrianberta08@gmail.com>
2025-04-28 11:13:27 +0200

    Merge branch 'main' of https://github.com/spumacej/posuvne_menu_test

--- static/menu.js
+++ static/menu.js
@@ -3,12 +3,15 @@
 let menu_default = document.getElementById("menu_default");
 
 window.addEventListener("scroll", function () {
-    if (window.scrollY > 99) {
-        menu_default.style.opacity = "1";
-        menu_default.style.transform = "translateY(0)";
-    } else {
-        menu_default.style.opacity = "0";
-        menu_default.style.transform = "translateY(-50px)";
+    if (menu_clicked.style.opacity == 0)
+    {    
+        if (window.scrollY > 64) {
+            menu_default.style.opacity = "1";
+            menu_default.style.transform = "translateY(0)";
+        } else {
+            menu_default.style.opacity = "0";
+            menu_default.style.transform = "translateY(-70px)";
+        }
     }
 });
 
@@ -28,3 +31,60 @@ window.addEventListener("scroll", function (){
         obrazek2.style.left = "-300px";
     }
 });
+
+// Zmiznutie textu logo
+
+let logotext = document.getElementById("logo_text");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 64) {
+        logotext.style.opacity = "0";
+        logotext.style.left = "0px";
+        logotext.style.transition = "opacity 0.6s ease-out, left 0.5s ease-in";
+    } else {
+        logotext.style.opacity = "1";
+        logotext.style.left = "90px";
+        logotext.style.transition = "opacity 0.6s ease, left 0.5s ease-out";
+    }
+});
+
+// Zmiznutie menu
+
+let menu = document.getElementById("menu");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 64) {
+        menu.style.opacity = "0";
+    } else {
+        menu.style.opacity = "80%";
+    }
+});
+
+
+
+
+
+
+// na to pod timhle komentarem nesahej pls 
+
+
+let menu_default_under = document.getElementById("menu_default_under");
+let menu_clicked = document.getElementById("menu_clicked")
+
+function click_menu_default ()
+{
+    menu_default.style.zIndex = "0";
+    menu_clicked.style.zIndex = "1";
+    menu_default_under.style.opacity = "1";
+    menu_default.style.opacity = "0";
+    menu_clicked.style.opacity = "1";
+    
+}
+
+function click_menu_cliked (){
+    menu_default.style.zIndex = "1";
+    menu_clicked.style.zIndex = "0";
+    menu_default_under.style.opacity = "0";
+    menu_default.style.opacity = "1";
+    menu_clicked.style.opacity = "0";
+}
